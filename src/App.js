3f781df5e81f2099e2d1b7cd0356fcc873f9d00b
Alejandro <alejandro.becerra.pro@gmail.com>
2021-02-26 10:58:35 -0500

    creando formulario

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {Fragment} from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
@@ -7,6 +8,20 @@ function App() {
         <Header
           titulo = 'Clima Ciudades - React con API'
         />
+        <div className="contenedor-form">
+          <div className="container">
+            <div className="row">
+              <div className="col m6 s12">
+                <Formulario
+                
+                />
+              </div>
+              <div className="col m6 s12">
+                2
+              </div>
+            </div>
+          </div>
+        </div>
     </Fragment>
     
   );
